db07d7858090beba48c6539fed8ecca4f5f773f7
Nicola Jordan <njordan.hsr@gmail.com>
2016-08-25 10:36:49 +0200

    illustrate failure of concatenating more than two search queries

--- tests/postgres_tests/test_search.py
+++ tests/postgres_tests/test_search.py
@@ -182,7 +182,7 @@ class MultipleFieldsTest(GrailTestData, PostgreSQLTestCase):
 
 @modify_settings(INSTALLED_APPS={'append': 'django.contrib.postgres'})
 class TestCombinations(GrailTestData, PostgreSQLTestCase):
-
+    maxDiff = None
     def test_vector_add(self):
         searched = Line.objects.annotate(
             search=SearchVector('scene__setting') + SearchVector('character__name'),
@@ -205,9 +205,24 @@ class TestCombinations(GrailTestData, PostgreSQLTestCase):
         ).filter(search=SearchQuery('bedemir') & SearchQuery('scales'))
         self.assertSequenceEqual(searched, [self.bedemir0])
 
+    def test_query_multiple_and(self):
+        searched = Line.objects.annotate(
+            search=SearchVector('scene__setting', 'dialogue'),
+        ).filter(search=SearchQuery('bedemir') & SearchQuery('scales') & SearchQuery('nostrils'))
+        self.assertSequenceEqual(searched, [])
+
+        searched = Line.objects.annotate(
+            search=SearchVector('scene__setting', 'dialogue'),
+        ).filter(search=SearchQuery('shall') & SearchQuery('use') & SearchQuery('larger'))
+        self.assertSequenceEqual(searched, [self.bedemir0])
+
     def test_query_or(self):
         searched = Line.objects.filter(dialogue__search=SearchQuery('kneecaps') | SearchQuery('nostrils'))
         self.assertSequenceEqual(set(searched), {self.verse1, self.verse2})
+
+    def test_query_multiple_or(self):
+        searched = Line.objects.filter(dialogue__search=SearchQuery('kneecaps') | SearchQuery('nostrils') | SearchQuery('Sir Robin'))
+        self.assertSequenceEqual(set(searched), {self.verse1, self.verse2, self.verse0})
 
     def test_query_invert(self):
         searched = Line.objects.filter(character=self.minstrel, dialogue__search=~SearchQuery('kneecaps'))
